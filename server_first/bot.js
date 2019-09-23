e63683bee08ac8c3e955db257b6136c0f367cf65
CubeRuben <egor.petrorov.03@mail.ru>
2019-09-23 18:36:08 +0300

    2 new bots

---
+++ server_first/bot.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const login = require("./login.json");
+const fs = require("fs");
+
+const bot = new Discord.Client();
+
+function checkNumberOfPlayers() 
+{
+    let numberOfPlayers = fs.readFileSync("../../server7777.txt");
+    console.log(numberOfPlayers + " of 30");
+    bot.user.setActivity(", что " + numberOfPlayers + " из 30 игроков на сервере", {type: "WATCHING"} );
+}
+
+bot.on('ready', () => {
+    console.log("Server bot loaded");
+    checkNumberOfPlayers() 
+    setInterval(() => checkNumberOfPlayers(), 10000);
+});
+
+bot.login(login.token);
